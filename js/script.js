a3def4ae17e79a670038a8e74e2d5bfad376918b
brunamundg <bruna.7628771@aluno.mg.gov.br>
2026-04-28 17:28:41 +0000

    descurit

--- js/script.js
+++ js/script.js
@@ -16,4 +16,52 @@ function curtir() {
   
 }
 
+documet.getElementById("likeBtn").addEventListener("click", curtir);
+
+let likeCount = 0;
+let curtido = false;
+
+let dislikeCount = 0;
+let descurtido = false;
+
+function curtir() {
+  if (curtido == false) {
+    
+    if (descurtido == true) {
+      descurtido = false;
+      dislikeCount--;
+    }
+    likeCount++;
+    curtido = true;
+  } else {
+    
+    likeCount--;
+    curtido = false;
+  }
+  atualizarInterface();
+}
+
+function descurtir() {
+  if (descurtido == false) {
+    
+    if (curtido == true) {
+      curtido = false;
+      likeCount--;
+    }
+    dislikeCount++;
+    descurtido = true;
+  } else {
+    
+    dislikeCount--;
+    descurtido = false;
+  }
+  atualizarInterface();
+}
+
+function atualizarInterface() {
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("dislikeBtn").addEventListener("click", descurtir);
