0e47a124b619fc0589ca65c59a73765552e75511
brunamundg <bruna.7628771@aluno.mg.gov.br>
2026-04-28 17:32:41 +0000

    função descurtir

--- js/script.js
+++ js/script.js
@@ -25,42 +25,39 @@ let dislikeCount = 0;
 let descurtido = false;
 
 function curtir() {
-  if (curtido == false) {
-    
-    if (descurtido == true) {
-      descurtido = false;
-      dislikeCount--;
-    }
-    likeCount++;
-    curtido = true;
-  } else {
-    
-    likeCount--;
-    curtido = false;
-  }
-  atualizarInterface();
+if (curtido == false) { 
+ if (descurtido == true) {
+ descurtido = false;
+ dislikeCount--;
+ }
+likeCount++;
+ curtido = true;
+ } else {
+ 
+ likeCount--;
+ curtido = false;
+ }
+ atualizarInterface();
 }
 
 function descurtir() {
-  if (descurtido == false) {
-    
-    if (curtido == true) {
-      curtido = false;
-      likeCount--;
-    }
-    dislikeCount++;
-    descurtido = true;
-  } else {
-    
-    dislikeCount--;
-    descurtido = false;
-  }
-  atualizarInterface();
+ if (descurtido == false) { 
+ if (curtido == true) {
+ curtido = false;
+likeCount--;
+ }
+ dislikeCount++;
+ descurtido = true;
+ } else {
+dislikeCount--;
+descurtido = false;
+ }
+ atualizarInterface();
 }
 
 function atualizarInterface() {
-  document.getElementById("likeCount").innerText = likeCount;
-  document.getElementById("dislikeCount").innerText = dislikeCount;
+document.getElementById("likeCount").innerText = likeCount;
+ document.getElementById("dislikeCount").innerText = dislikeCount;
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
